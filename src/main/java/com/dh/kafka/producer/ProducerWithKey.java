0cec3cfe7f2bf8d777a0d0babd383374093d5c2e
psaradhi <parthasaradhi.malayathi@dharbor.com>
2021-08-26 20:06:03 +0530

    producer sent msg with key

---
+++ src/main/java/com/dh/kafka/producer/ProducerWithKey.java
@@ -0,0 +1,59 @@
+package com.dh.kafka.producer;
+
+import com.dh.kafka.config.Key;
+import com.dh.kafka.config.Value;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class Producer {
+
+    public static void main(String[] args) {
+
+        KafkaProducer<String,String> producer = null;
+        try {
+            Properties properties = new Properties();
+            properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+            properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+            properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+            producer = new KafkaProducer<String, String>(properties);
+
+
+            ProducerRecord msg = new ProducerRecord("test2topic", "m8");
+            producer.send(msg, (RecordMetadata recordMetadata, Exception e) -> {
+                if (e == null){
+                    long offset = recordMetadata.offset();
+                    int partition = recordMetadata.partition();
+                    System.out.println("offset -> "+offset + " partition -> "+partition);
+                    System.out.println("msg is sent");
+                }
+                else {
+                    System.out.println("msg sent is failed");
+                }
+
+            } );
+
+            ProducerRecord msg2 = new ProducerRecord("test2topic", "id1", "m9");
+            producer.send(msg2, (RecordMetadata recordMetadata, Exception e) -> {
+                if (e == null){
+                    long offset = recordMetadata.offset();
+                    int partition = recordMetadata.partition();
+                    System.out.println("offset -> "+offset + " partition -> "+partition);
+                    System.out.println("msg2 is sent");
+                }
+                else {
+                    System.out.println("msg sent is failed");
+                }
+
+            } );
+
+            producer.flush();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            producer.close();
+        }
+    }
+}
